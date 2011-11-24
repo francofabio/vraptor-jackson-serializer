993c9db303ca8150cd09c8ec510612d2ba9ddf1e
Fabio Franco <francofabio@gmail.com>
2011-11-24 11:31:27 -0200

    Add logs in deserializer

--- src/main/java/com/github/francofabio/vraptor/jackson/deserialization/JacksonDeserialization.java
+++ src/main/java/com/github/francofabio/vraptor/jackson/deserialization/JacksonDeserialization.java
@@ -1,5 +1,6 @@
 package com.github.francofabio.vraptor.jackson.deserialization;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.text.SimpleDateFormat;
@@ -7,6 +8,8 @@ import java.text.SimpleDateFormat;
 import org.codehaus.jackson.JsonNode;
 import org.codehaus.jackson.map.DeserializationConfig;
 import org.codehaus.jackson.map.ObjectMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import br.com.caelum.vraptor.deserialization.Deserializer;
 import br.com.caelum.vraptor.deserialization.Deserializes;
@@ -17,6 +20,8 @@ import br.com.caelum.vraptor.view.ResultException;
 @Deserializes({ "application/json", "json" })
 public class JacksonDeserialization implements Deserializer {
 
+	private static final Logger logger = LoggerFactory.getLogger(JacksonDeserialization.class);
+	
     private final ParameterNameProvider paramNameProvider;
     
     public JacksonDeserialization(ParameterNameProvider paramNameProvider) {
@@ -36,7 +41,9 @@ public class JacksonDeserialization implements Deserializer {
         String[] parameterNames = paramNameProvider.parameterNamesFor(jMethod);
         
         try {
-            JsonNode root = mapper.readTree(inputStream);
+        	String content = getContentOfStream(inputStream);
+        	logger.debug("json retrieved: " + content);
+            JsonNode root = mapper.readTree(content);
             
             for (int i=0; i < types.length; i++) {
                 String name = parameterNames[i];
@@ -63,6 +70,18 @@ public class JacksonDeserialization implements Deserializer {
         mapper.setDateFormat(sdf);
         
         return mapper;
+    }
+    
+    private String getContentOfStream(InputStream input) throws IOException {
+    	StringBuilder content = new StringBuilder();
+    	
+    	byte[] buffer = new byte[1024];
+    	int readed = 0;
+    	while ((readed=input.read(buffer)) != -1) {
+    		content.append(new String(buffer, 0, readed));
+    	}
+    	
+    	return content.toString();
     }
 
 }
